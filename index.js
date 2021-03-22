92e9fdd44407249817b86bc8b44f606698c96f42
Fernando Garcia <jfer.garciav@gmail.com>
2021-03-21 19:34:22 -0600

    Base files and dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+// Libraries
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+clear();
+const divider = Array(40).fill('*').join(' ');
+const title = figlet.textSync('H A N G M A N');
+const titleColor = chalk.yellow(`${divider}\n${title}\n${divider}`);
+console.log(titleColor);
