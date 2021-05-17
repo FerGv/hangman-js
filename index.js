5dd5008eaa31f1b2630a36aae1e5fc58d067e5fb
Fernando Garcia <jfer.garciav@gmail.com>
2021-05-16 19:32:36 -0500

    Complete game

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const clear = require('clear');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
 
+const human = require('./human');
+
 function logMultiplier(number, symbolFill = '*', symbolJoin = ' ') {
   return Array(number).fill(symbolFill).join(symbolJoin);
 }
@@ -17,17 +19,53 @@ async function main() {
 
   const SECRET_WORD = 'curtain';
   const SECRET_WORD_LENGTH = SECRET_WORD.length;
+  const discoveredLetters = [];
+  let errors = 0;
 
   const wordSpaces = logMultiplier(SECRET_WORD_LENGTH, '_');
-  console.log(wordSpaces);
+  console.log(`\nSECRET WORD: ${wordSpaces}\n`);
 
-  const answers = await inquirer.prompt([
-    {
-      name: 'letter',
-      message: 'Ingresa una letra: ',
-    },
-  ]);
-  console.log(answers);
+  do {
+    let { letter } = await inquirer.prompt([
+      {
+        name: 'letter',
+        message: 'Ingresa una letra: ',
+      },
+    ]);
+    letter = letter.toLowerCase();
+
+    if (SECRET_WORD.includes(letter)) {
+      if (discoveredLetters.includes(letter)) {
+        console.log(chalk.red('\nLETRA REPETIDA.'));
+        errors += 1;
+        console.log(chalk.red(`\n${human[errors]}`));
+      }
+
+      discoveredLetters.push(letter);
+    } else {
+      console.log(chalk.red('\nLETRA ERRÓNEA.'));
+      errors += 1;
+      console.log(chalk.red(`\n${human[errors]}`));
+    }
+
+    if (errors === 6) {
+      const loserMessage = figlet.textSync('YA PERDISTE : c');
+      console.log(chalk.red(`\n${loserMessage}`));
+      break;
+    }
+
+    const secretWithDiscovered = Array.from(SECRET_WORD).map((letter) =>
+      discoveredLetters.includes(letter) ? letter : '_',
+    );
+
+    console.log(`\nSECRET WORD: ${secretWithDiscovered.join(' ')}\n`);
+
+    if (secretWithDiscovered.join('') === SECRET_WORD) {
+      const loserMessage = figlet.textSync('YA GANASTE c :');
+      console.log(chalk.green(`\n${loserMessage}`));
+      break;
+    }
+  } while (true);
 }
 
 main();
